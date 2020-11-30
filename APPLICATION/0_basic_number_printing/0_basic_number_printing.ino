7f493cdb62e19aea5b904ee4b450db4b1177fb53
adarshkumarsingh83 <adarshkumarsingh83@gmail.com>
2020-11-30 14:46:45 -0600

    adding

--- APPLICATION/0_basic_number_printing/0_basic_number_printing.ino
+++ APPLICATION/0_basic_number_printing/0_basic_number_printing.ino
@@ -1,12 +1,13 @@
 
+//binary literal
+byte binaryCounter = B00000000;
+
 //deciaml literal
 int decimalCounter = 0;
 
 //octal literal
 byte octalCounter = 00;
 
-//binary literal
-byte binaryCounter = B00000000;
 
 //hexa deciaml literal
 byte hexaDeciamlCounter = 0x0;
@@ -17,54 +18,82 @@ void setup() {
 }
 
 void loop() {
+  Serial.println();
   //binary number
+  Serial.print("Binary \t");
   Serial.print(decimalCounter, BIN);
   Serial.print("\t");
   //octal number
+  Serial.print("Octal \t");
   Serial.print(decimalCounter, OCT);
   Serial.print("\t");
+  //decimal number
+  Serial.print("Decimal \t");
+  Serial.print(decimalCounter, DEC);
+  Serial.print("\t");
   // hexa decimal number
+  Serial.print("HexaDecimal \t");
   Serial.print(decimalCounter, HEX);
   Serial.println();
 
 
-    //binary number
+  //binary number
+  Serial.print("Binary \t");
   Serial.print(binaryCounter, BIN);
   Serial.print("\t");
   //octal number
+  Serial.print("Octal \t");
   Serial.print(binaryCounter, OCT);
   Serial.print("\t");
+  //decimal number
+  Serial.print("Decimal \t");
+  Serial.print(binaryCounter, DEC);
+  Serial.print("\t");
   // hexa decimal number
+  Serial.print("HexaDecimal \t");
   Serial.print(binaryCounter, HEX);
   Serial.println();
 
 
-      //binary number
+  //binary number
+  Serial.print("Binary \t");
   Serial.print(octalCounter, BIN);
   Serial.print("\t");
   //octal number
+  Serial.print("Octal \t");
   Serial.print(octalCounter, OCT);
   Serial.print("\t");
+  //decimal number
+  Serial.print("Decimal \t");
+  Serial.print(octalCounter, DEC);
+  Serial.print("\t");
   // hexa decimal number
+  Serial.print("HexaDecimal \t");
   Serial.print(octalCounter, HEX);
   Serial.println();
 
-  
-    //binary number
+
+  //binary number
+  Serial.print("Binary \t");
   Serial.print(hexaDeciamlCounter, BIN);
   Serial.print("\t");
   //octal number
+  Serial.print("Octal \t");
   Serial.print(hexaDeciamlCounter, OCT);
   Serial.print("\t");
+  //decimal number
+  Serial.print("Decimal \t");
+  Serial.print(hexaDeciamlCounter, DEC);
+  Serial.print("\t");
   // hexa decimal number
+  Serial.print("HexaDecimal \t");
   Serial.print(hexaDeciamlCounter, HEX);
-  Serial.println();
   Serial.println();
 
   binaryCounter++;
   decimalCounter++;
   octalCounter++;
   hexaDeciamlCounter++;
-  
+
   delay(DELAY_TIME);
 }
