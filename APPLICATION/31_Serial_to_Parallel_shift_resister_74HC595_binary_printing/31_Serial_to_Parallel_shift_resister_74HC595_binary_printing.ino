6327973cf45655c2a8c24e4b440aa332ab8887c8
adarshkumarsingh83 <adarshkumarsingh83@gmail.com>
2020-11-30 18:04:13 -0600

    adding

--- APPLICATION/31_Serial_to_Parallel_shift_resister_74HC595_binary_printing/31_Serial_to_Parallel_shift_resister_74HC595_binary_printing.ino
+++ APPLICATION/31_Serial_to_Parallel_shift_resister_74HC595_binary_printing/31_Serial_to_Parallel_shift_resister_74HC595_binary_printing.ino
@@ -7,6 +7,8 @@ const int clockPin = 9;
 
 byte LEDS_ON = 0x00;
 byte LEDS_OFF = 0xFF;
+
+byte counter = 0b0;
 void setup() {
   Serial.begin(9600);
   pinMode(dataPin, OUTPUT);
@@ -15,12 +17,20 @@ void setup() {
 }
 
 void loop() {
+
+  if (counter < 255) {
+    Serial.println(counter, BIN);
     digitalWrite(latchPin, LOW);
-    shiftOut(dataPin, clockPin, LSBFIRST, LEDS_ON);
+    shiftOut(dataPin, clockPin, LSBFIRST, counter);
     digitalWrite(latchPin, HIGH);
-    delay(1000);
+    delay(5000);
     digitalWrite(latchPin, LOW);
     shiftOut(dataPin, clockPin, LSBFIRST, LEDS_OFF);
     digitalWrite(latchPin, HIGH);
     delay(1000);
+  } else {
+    delay(10000);
+    counter = 0;
+  }
+  counter++;
 }
---
+++ APPLICATION/31_Serial_to_Parallel_shift_resister_74HC595_binary_printing/README.md
@@ -0,0 +1,8 @@
+# Serial to Parallel Shift Register (74HC595)
+
+---
+
+in 74HC595 q0 to q7 are output pin for device  \
+pin 9 is clock pin which will synch the data across the 74HC595 \
+pin 12 is latch pin when data is written latch has to be low then write data and again make it high\
+pin 14 is he data pin where we will write the data as instruction for devices \
