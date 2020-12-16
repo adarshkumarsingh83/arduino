c28b107ac920f6af187a704e87bfbdbdedad8bbc
adarshkumarsingh83 <adarshkumarsingh83@gmail.com>
2020-12-15 22:24:51 -0600

    adding

---
+++ APPLICATION/36_infrared_sensor_remote/36_infrared_sensor_remote.ino
@@ -0,0 +1,22 @@
+#include <IRremote.h>
+
+const int RECV_PIN = 11;
+
+IRrecv irrecv(RECV_PIN);
+
+decode_results results;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Enabling IRin");
+  irrecv.enableIRIn(); // Start the receiver
+  Serial.println("Enabled IRin");
+}
+
+void loop() {
+  if (irrecv.decode(&results)) {
+    Serial.println(results.value, HEX);
+    irrecv.resume(); // Receive the next value
+  }
+  delay(100);
+}
