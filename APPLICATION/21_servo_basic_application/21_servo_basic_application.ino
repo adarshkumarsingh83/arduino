f8676675388c7e26c94a264d1070bdd1755779c3
adarshkumarsingh83 <adarshkumarsingh83@gmail.com>
2021-01-13 09:18:10 -0600

    updating

--- APPLICATION/21_servo_basic_application/21_servo_basic_application.ino
+++ APPLICATION/21_servo_basic_application/21_servo_basic_application.ino
@@ -2,22 +2,52 @@
 
 int servoPin = 13;
 int servoPosition = 0;
+String direction = "F";
+bool initCheckVar = true;
 Servo myServoMoter;
 
 void setup() {
-   Serial.begin(9600);
+  Serial.begin(9600);
   pinMode(servoPin, OUTPUT);
   myServoMoter.attach(servoPin);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  
-   myServoMoter.write(servoPosition);
-   if(servoPosition == 180){
-     servoPosition = 0;
-   }else{
-     servoPosition ++;
-   }
-   delay(500);
+  if (initCheckVar) {
+    initCheckVar = initCheck(initCheckVar);
+  }
+
+  myServoMoter.write(servoPosition);
+  Serial.print("DIRECTION IS ");
+  Serial.print(direction);
+  Serial.print(" ");
+  Serial.println(servoPosition);
+  if (direction == "F") {
+    servoPosition ++;
+    if (servoPosition == 180) {
+      direction = "B";
+      Serial.println("DIRECTION IS CHANGING TO BACKWARD");
+    }
+  } else {
+    servoPosition --;
+    if (servoPosition == 0) {
+      direction = "F";
+      Serial.println("DIRECTION IS CHANGING TO FORWARD");
+    }
+  }
+
+  delay(200);
+}
+
+bool initCheck(bool initCheckVar) {
+  Serial.println("initCheck executed");
+  myServoMoter.write(0);
+  delay(100);
+  myServoMoter.write(90);
+  delay(100);
+  myServoMoter.write(180);
+  delay(100);
+  myServoMoter.write(90);
+  initCheckVar = false;
+  return initCheckVar;
 }
