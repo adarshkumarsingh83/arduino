9e6611cf6d3dfae9d59386d222673846caea4146
adarshkumarsingh83 <adarshkumarsingh83@gmail.com>
2021-02-06 12:14:21 -0600

    arduino-pcf8574-ir-intrrupt-example

--- APPLICATION/arduino-pcf8574-ir-intrrupt-example/arduino-pcf8574-ir-intrrupt-example.ino
+++ APPLICATION/arduino-pcf8574-ir-intrrupt-example/arduino-pcf8574-ir-intrrupt-example.ino
@@ -1,9 +1,7 @@
 #include "Arduino.h"
 #include "PCF8574.h"
 
-#define INTERRUPTED_PIN 2
-
-bool irActive = false;
+const byte INTERRUPTED_PIN = 2;
 
 // Function interrupt
 void infraRedActive();
@@ -12,44 +10,52 @@ void infraRedActive();
 PCF8574 pcf8574(0x20, INTERRUPTED_PIN, infraRedActive);
 
 
+bool irActive = false;
+void interrupt_routine();
+uint8_t val = 0;
+
 void setup() {
+
   Serial.begin(9600);
+
   // Set pinMode to OUTPUT
   pcf8574.pinMode(P0, OUTPUT);
   pcf8574.pinMode(P1, OUTPUT);
+  pcf8574.pinMode(P2, INPUT);
   pcf8574.pinMode(P3, INPUT);
 
   Serial.print("Init pcf8574...");
   if (pcf8574.begin()) {
-    Serial.println("INIT OK");
+    Serial.println("OK");
   } else {
-    Serial.println("INIT FAILED");
+    Serial.println("KO");
   }
 }
 
 void loop() {
-  if (irActive) {
+  pcf8574.digitalWrite(P0, LOW);
+
+  if (irActive == true) {
     pcf8574.digitalWrite(P0, HIGH);
     pcf8574.digitalWrite(P1, LOW);
-    uint8_t val = pcf8574.digitalRead(P3);
-    Serial.print("READ VALUE FROM PCF ");
-    Serial.println(val);
-     delay(1000);
+    val = pcf8574.digitalRead(P2);
+    if (val == 0) {
+      Serial.print("READ VALUE FROM PCF P2 ");
+      Serial.println(val);
+    }
+    val = pcf8574.digitalRead(P3);
+    if (val == 0) {
+      Serial.print("READ VALUE FROM PCF P3 ");
+      Serial.println(val);
+    }
+    delay(1000);
     irActive = false;
-  } else {
     pcf8574.digitalWrite(P1, HIGH);
-
-    pcf8574.digitalWrite(P0, HIGH);
-    delay(1000);
-    pcf8574.digitalWrite(P0, LOW);
-    delay(1000);
   }
+
 }
 
 void infraRedActive() {
-  /*
-    Interrupt called (No Serial no read no wire in this
-    function, and DEBUG disabled on PCF library)
-  */
+  //Serial.println("*");
   irActive = true;
 }
