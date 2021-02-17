200e0a7a88aacc208c4335ed477678695e02b364
adarshkumarsingh83 <adarshkumarsingh83@gmail.com>
2021-02-17 12:06:29 -0600

    esp8266-wifi-connection and esp8266-http-server

---
+++ APPLICATION/esp8266-web-server-led-app/README.md
@@ -0,0 +1,14 @@
+# esp8266 
+
+---
+
+### connection details 
+```
+connwect the gnd from esp8266 to the breadboard gnd 
+connect the D1 and D2 to the  2 risistor on the breaadboard 
+and other end of the resisitor will join the long led of the 1st and 2nd led on breadboard 
+and short 1st and 2nd led of the led will join the gnd of the bredboard 
+then the url http://<ip show in the console> 
+then ui must be pop up for the on and off button 
+
+```
--- APPLICATION/esp8266-wifi-connection/esp8266-wifi-connection.ino
+++ APPLICATION/esp8266-wifi-connection/esp8266-wifi-connection.ino
@@ -1,40 +1,39 @@
 #include <ESP8266WiFi.h>
 
-#ifndef APSSID
-#define APSSID "*****"
-#define APPSK  "*****"
-#endif
-
 /* Set these to your desired credentials. */
-const char *ssid = APSSID;
-const char *password = APPSK;
+const char* ssid = "REPLACE_WITH_YOUR_SSID";
+const char* password = "REPLACE_WITH_YOUR_PASSWORD";
 
 void setup() {
-  Serial.begin(9600);
+  
+  pinMode(LED_BUILTIN,OUTPUT);
+  digitalWrite(LED_BUILTIN,HIGH);
 
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, LOW);
-
+  Serial.begin(115200);
   Serial.println();
   Serial.print("Wifi connecting to ");
   Serial.println( ssid );
 
-  WiFi.begin(ssid, password);
+  WiFi.begin(ssid,password);
+
   Serial.println();
   Serial.print("Connecting");
+
+  while( WiFi.status() != WL_CONNECTED ){
+      delay(500);
+      Serial.print(".");        
+  }
+
+  digitalWrite( LED_BUILTIN , LOW);
+  Serial.println();
+
+  Serial.println("Wifi Connected Success!");
+  Serial.print("NodeMCU IP Address : ");
+  Serial.println(WiFi.localIP() );
+
 }
 
 void loop() {
+  // put your main code here, to run repeatedly:
 
-  if ( WiFi.status() != WL_CONNECTED ) {
-    digitalWrite( LED_BUILTIN , HIGH);
-    Serial.println();
-    Serial.println("Wifi Connected Success!");
-    Serial.print("NodeMCU IP Address : ");
-    Serial.println(WiFi.localIP() );
-    delay(1000);    
-    Serial.print(".");
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(1000);    
-  }
 }
