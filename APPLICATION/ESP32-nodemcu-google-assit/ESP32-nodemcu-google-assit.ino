4842228131be23c474aa4ea4805176e0d2b46f16
adarshkumarsingh83 <adarshkumarsingh83@gmail.com>
2021-02-17 22:01:41 -0600

    ESP32-nodemcu-google-assit

---
+++ APPLICATION/ESP32-nodemcu-google-assit/ESP32-nodemcu-google-assit.ino
@@ -0,0 +1,26 @@
+#define BLYNK_PRINT Serial
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "AUTH-TOKEN-FROM-BLYNK";
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "WIFI-NAME";
+char pass[] = "WIFI-PASSWORD";
+
+void setup() {
+  // Debug console
+  Serial.begin(9600);
+
+  Blynk.begin(auth, ssid, pass);
+  // You can also specify server:
+  //Blynk.begin(auth, ssid, pass, "blynk-cloud.com", 80);
+  //Blynk.begin(auth, ssid, pass, IPAddress(192,168,1,100), 8080);
+}
+
+void loop() {
+  Blynk.run();
+}
