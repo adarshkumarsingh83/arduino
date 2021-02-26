0de415bd0f67c67913a53e004e5685628fdee735
adarshkumarsingh83 <adarshkumarsingh83@gmail.com>
2021-02-26 15:39:44 -0600

    adding queue

--- APPLICATION/queue-example/queue-example.ino
+++ APPLICATION/queue-example/queue-example.ino
@@ -1,42 +1,43 @@
+#include "Queue.h"
+
 /*
-  {"type":76,"pin":5,"state":1}
-  {"type":76,"pin":5,"state":1,"opStatus":"ON"}
-
-  {"type":76,"pin":5,"state":0}
-  {"type":76,"pin":5,"state":0,"opStatus":"OFF"}
-
-  {"type":76,"pin":4,"state":1}
-  {"type":76,"pin":4,"state":1,"opStatus":"ON"}
-
-  {"type":76,"pin":4,"state":0}
-  {"type":76,"pin":4,"state":0,"opStatus":"OFF"}
+   search the lib Arduino-Queue.h in lib manager
 */
 
-boolean newData = false;
+String serialData = "";
+
+#define QUEUE_SIZE_ITEMS 10
+
+Queue<String> queue = Queue<String>();
+bool dataAvalable = false;
 const byte numChars = 32;
 const  char startMarker = '{';
 const  char endMarker = '}';
 char receivedChars[numChars];
-
 void setup() {
+  // Init Serial Monitor
   Serial.begin(9600);
-  Serial.println("<Arduino is ready>");
 }
 
 void loop() {
-  if (Serial.available() > 0 ) {
-    String data = recvWithStartEndMarkers();
-    showNewData(data);
+  if (Serial.available()) {
+
+    serialData = recivedSerialJsonData();
+    if (queue.count() != 256) {
+      queue.push(serialData);
+    } else {
+      String data = queue.pop();
+      Serial.println(data);
+    }
   }
 }
 
-
-String recvWithStartEndMarkers() {
+String recivedSerialJsonData() {
   static boolean recvInProgress = false;
   static byte ndx = 0;
   char rc;
   String data;
-  while (Serial.available() > 0 && newData == false) {
+  while (Serial.available() > 0 && Serial == false) {
     rc = Serial.read();
     if (recvInProgress == true) {
       if (rc != endMarker) {
@@ -51,7 +52,7 @@ String recvWithStartEndMarkers() {
         receivedChars[ndx] = '\0'; // terminate the string
         recvInProgress = false;
         ndx = 0;
-        newData = true;
+        dataAvalable = true;
       }
     } else if (rc == startMarker) {
       receivedChars[ndx] = '{';
@@ -61,12 +62,4 @@ String recvWithStartEndMarkers() {
   }
   data = receivedChars;
   return  data;
-}
-
-void showNewData(String data) {
-  if (newData == true) {
-    Serial.print("This just in ... ");
-    Serial.println(data);
-    newData = false;
-  }
 }
