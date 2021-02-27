39b69c3cf82d3f30c0dde01309c788a63cae293b
adarshkumarsingh83 <adarshkumarsingh83@gmail.com>
2021-02-26 21:35:25 -0600

    ading custom queue lib circular-queue-lib

---
+++ APPLICATION/circular-queue-lib/README.md
@@ -0,0 +1,8 @@
+## CircularQueue
+
+----
+
+```
+copy the "circular-queue-lib" dir in the arduino lib dir and use it in code
+
+```
---
+++ APPLICATION/circular-queue-lib/circular-queue-example/circular-queue-example.ino
@@ -0,0 +1,23 @@
+#include "CircularQueue.h"
+
+const CircularQueue<int> circularQueueInt = CircularQueue<int>(5, "int-queue");
+const CircularQueue<String> circularQueueString = CircularQueue<String>(5, "string-queue");
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  delay(1000);
+  if (!circularQueueInt.push(random(300))) {
+    circularQueueInt.displayQueue();
+    circularQueueInt.resetQueue();
+  }
+
+  String d = "adi_";
+  d += random(100);
+  if (!circularQueueString.push(d)) {
+    circularQueueString.displayQueue();
+    circularQueueString.resetQueue();
+  }
+}
