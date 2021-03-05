6aa9221f7a74838f65043094f4ed1967b4bf26cd
adarshkumarsingh83 <adarshkumarsingh83@gmail.com>
2021-03-04 19:42:53 -0600

    adding spi-nodemcu-uno-master-slave-string-bidirection-communication

---
+++ APPLICATION/spi-nodemcu-uno-master-slave-string-bidirection-communication/README.md
@@ -0,0 +1,27 @@
+# SPI NODE MCU AS MASTER AND ARDUINO UNO SLAVE BI-DIRECTION STRING DATA COMMUNICATION 
+
+---
+
+## connectionss 
+
+![img](https://github.com/adarshkumarsingh83/arduino/blob/master/APPLICATION/spi-nodemcu-uno-master-slave-string-bidirection-communication/image/connections.JPG)
+
+
+![img](https://github.com/adarshkumarsingh83/arduino/blob/master/APPLICATION/spi-nodemcu-uno-master-slave-string-bidirection-communication/image/spi-arduino-uno-connections.jpg)
+
+```
+connect pin 13 of  uno to the pin D5 of Node mcu
+connect pin 12 of  uno to the pin D6 of Node mcu
+connect pin 13 of  uno to the pin D7 of Node mcu
+connect pin 10 of  uno to the pin D8 of Node mcu
+connect the gnd of  uno to the gnd of the Node mcu 
+
+```
+
+
+### To Open Two arduino sperate ide on mac
+* $ open -n -a Arduino
+	* -n = open new instance even when one is already running
+	* -a xxx = open application xxx
+
+![img](https://github.com/adarshkumarsingh83/arduino/blob/master/APPLICATION/spi-nodemcu-uno-master-slave-string-bidirection-communication/image/serial-monitors.png)
---
+++ APPLICATION/spi-nodemcu-uno-master-slave-string-bidirection-communication/spi-arduino-slave-bi-direction/spi-arduino-slave-bi-direction.ino
@@ -0,0 +1,80 @@
+/*
+  PIN FOR COMMUNICATION IN NODE MCU
+  SCK =  D5 GPIO14 (HSCKL)
+  MISO = D6 GPIO15 (HMISO)
+  MOSI = D7 GPIO16 (HMOSI)
+  SS =   D8 /GPIO 17(HCS)
+
+  PIN FOR COMMUNICATION IN ARDUINO UNO
+  MOSI = 11 or ICSP-4
+  MISO = 12 or ICSP-1
+  SCK = 13 or ICSP-3
+  SS = 10
+
+  PIN MAPPING
+  NODE MCU MASTER    UNO SLAVE
+  D5                  13
+  D6                  12
+  D7                  11
+  D8                  10
+  GND                 GND
+*/
+
+#include<SPI.h>
+
+const String MSG = " HI_RADHA_SINGH:";
+const int dataSize = MSG.length();
+
+char dataBuffer[20];
+volatile byte index;
+bool dataToReceived = false;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(MISO, OUTPUT);                  //Sets MISO as OUTPUT (Have to Send data to Master IN
+  SPCR |= _BV(SPE);                       //Turn on SPI in Slave Mode
+  dataToReceived = false;
+  SPI.attachInterrupt();
+  index = -1;
+}
+
+ISR (SPI_STC_vect) {
+  dataToReceived = true;
+}
+
+void loop() {
+  //Serial.print("SLAVE ");
+  //Serial.println(index);
+
+  if (index == -1) {
+    if (dataToReceived) {
+      char requestInit = SPDR;
+      if (requestInit == 'A') {
+        SPDR = requestInit;
+        index = 0;
+      }
+    }
+  } else if (index != -1 && index < dataSize) {
+    dataRecieved();
+  } else {
+    index = 0;
+    Serial.println(dataBuffer);
+    for (int i = 0; i < 20 ; i++) {
+      dataBuffer[i] = ' ';
+    }
+  }
+}
+
+void dataRecieved() {
+  if (dataToReceived) {
+    char received = SPDR;
+    if (received != ':') {
+      dataBuffer[index] = received;
+      SPDR =  MSG[index];
+      index += 1;
+    } else {
+      index = dataSize;
+    }
+    dataToReceived = false;
+  }
+}
