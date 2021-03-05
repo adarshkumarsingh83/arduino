e58ab07205eaf59c40fa8d55480dfb55a6a99e6d
adarshkumarsingh83 <adarshkumarsingh83@gmail.com>
2021-03-05 12:58:30 -0600

    adding spi-mega-master-uno-slave-string-bidirection-communication

---
+++ APPLICATION/spi-mega-master-uno-slave-string-bidirection-communication/README.md
@@ -0,0 +1,38 @@
+# SPI MEGA MASTER AND ARDUINO UNO SLAVE BI-DIRECTION COMMUNICATION 
+
+---
+
+## connectionss 
+
+![img](https://github.com/adarshkumarsingh83/arduino/blob/master/APPLICATION/spi-mega-master-uno-slave-string-bidirection-communication/image/connections.JPG)
+
+
+![img](https://github.com/adarshkumarsingh83/arduino/blob/master/APPLICATION/spi-mega-master-uno-slave-string-bidirection-communication/image/spi-mega-arduino-uno-connections.png)
+
+```
+connect pin 13 of  uno to the pin 52 of MEGA
+connect pin 12 of  uno to the pin 50 of MEGA
+connect pin 11 of  uno to the pin 51 of MEGA
+connect pin 10 of  uno to the pin 53 of MEGA
+connect the gnd of uno to the gnd of the MEGA
+
+then connect the gnd from uno the common breadboard gnd line 
+connect the pin 7 from uno to the breadboard 
+then connect the resistor to the pin 7 connect in breadboard 
+then connect the long leg of the led to the resistor 
+then connect the short leg of the led to the breadboard gnd line 
+
+
+connect the D1/gpio 5 pin from MEGA to the breadbboard 
+then connect the resitor to the D1/gpio 5 pin in breadboard 
+then connect the long leg of the led to the resistor 
+then connect the short leg of the led to the breadboard gnd line 
+```
+
+
+### To Open Two arduino sperate ide on mac
+* $ open -n -a Arduino
+	* -n = open new instance even when one is already running
+	* -a xxx = open application xxx
+
+![img](https://github.com/adarshkumarsingh83/arduino/blob/master/APPLICATION/spi-mega-master-uno-slave-string-bidirection-communication/image/serial-monitors.png)
---
+++ APPLICATION/spi-mega-master-uno-slave-string-bidirection-communication/spi-uno-slave-bi-direction/spi-uno-slave-bi-direction.ino
@@ -0,0 +1,85 @@
+/*
+  PIN FOR COMMUNICATION IN ARDUINO MEGA
+  MISO = 50 or ICSP-1
+  MOSI = 51 or ICSP-4
+  SCK = 52 or ICSP-3
+  SS = 53
+  
+  #PIN FOR COMMUNICATION IN ARDUINO UNO
+  MOSI = 11 or ICSP-4
+  MISO = 12 or ICSP-1
+  SCK = 13 or ICSP-3
+  SS = 10
+  
+  #PIN MAPPING
+  UNO SLAVE    MEGA MASTER
+  13               52
+  12               50
+  11               51
+  10               53
+  GND              GND
+*/
+
+#include<SPI.h>
+
+#define LED_PIN 7
+
+const String MSG = " HI_RADHA_SINGH:";
+const int dataSize = MSG.length();
+
+char dataBuffer[20];
+volatile byte index;
+bool dataToReceived = false;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(LED_PIN, OUTPUT);  
+  pinMode(MISO, OUTPUT);                  //Sets MISO as OUTPUT (Have to Send data to Master IN
+  SPCR |= _BV(SPE);                       //Turn on SPI in Slave Mode
+  dataToReceived = false;
+  SPI.attachInterrupt();
+  index = -1;
+}
+
+ISR (SPI_STC_vect) {
+  dataToReceived = true;
+}
+
+void loop() {
+  //Serial.print("SLAVE ");
+  //Serial.println(index);
+
+  if (index == -1) {
+    if (dataToReceived) {
+      char requestInit = SPDR;
+      if (requestInit == 'A') {
+        SPDR = requestInit;
+        index = 0;
+      }
+    }
+  } else if (index != -1 && index < dataSize) {
+    dataRecieved();
+    digitalWrite(LED_PIN, HIGH);  
+  } else {
+    index = 0;
+    digitalWrite(LED_PIN, LOW);  
+    Serial.println(dataBuffer);
+    for (int i = 0; i < 20 ; i++) {
+      dataBuffer[i] = ' ';
+    }
+  }
+}
+
+void dataRecieved() {
+  if (dataToReceived) {
+    char received = SPDR;
+    if (received != ':') {
+      dataBuffer[index] = received;
+      SPDR =  MSG[index];
+      index += 1;
+    } else {
+      index = dataSize;
+    }
+    dataToReceived = false;
+  }
+}
