6a68b804cad84fac5a42a4aa06f232dad56ed71c
adarshkumarsingh83 <adarshkumarsingh83@gmail.com>
2021-03-06 10:01:18 -0600

    adding pca9685 lib

--- CUSTOM-LIBRARY/pac9685-lib/pca9685/Pca9685.cpp
+++ CUSTOM-LIBRARY/pac9685-lib/pca9685/Pca9685.cpp
@@ -9,30 +9,61 @@
 #include "Pca9685.h"
 
 
-void Pca9685::initPca9685Board() {
-  Serial.begin(9600);
+void Pca9685::initPca9685() {
   //_pwm.begin();
-  for (int i = 0; i < 16; i++) {
+
+  if (_totalPins == -1) {
+    _totalPins = 16;
+  }
+  _pca9685PinList = new Pca9685Pin[_totalPins];
+  for (int i = 0; i < _totalPins; i++) {
     _pca9685PinList[i]._openState = 1000;
     _pca9685PinList[i]._closeState = 2000;
     _pca9685PinList[i]._isOpen = false;
   }
 }
 
-bool Pca9685::throwSwitch(int pinNo) {
+void Pca9685::setBoardAddress(int boardsAddress) {
+  this->_boardsAddress = boardsAddress;
+}
+
+int Pca9685::getBoardAddress() {
+  return this->_boardsAddress;
+}
+
+bool Pca9685::throwSwitchPca9685Pin(int pinNo) {
   _pca9685PinList[pinNo]._isOpen = true;
   Pca9685Pin pin = _pca9685PinList[pinNo];
   refreshPin(pinNo, pin);
   return true;
 }
 
-bool Pca9685::closeSwitch(int pinNo) {
+bool Pca9685::closeSwitchPca9685Pin(int pinNo) {
   _pca9685PinList[pinNo]._isOpen = false;
   Pca9685Pin pin = _pca9685PinList[pinNo];
   refreshPin(pinNo, pin);
   return true;
 }
 
+void Pca9685::resetPca9685Board() {
+  for (int i = 0; i < _totalPins; i++) {
+    Pca9685Pin pca9685Pin = _pca9685PinList[i];
+    pca9685Pin._isOpen = false;
+  }
+  refreshPca9685Board();
+}
+
+void Pca9685::refreshPca9685Board() {
+  for (int i = 0; i < _totalPins; i++) {
+    Pca9685Pin pca9685Pin = _pca9685PinList[i];
+    if (pca9685Pin._isOpen) {
+      //_pwm.setPWM(pinNo, 0, pca9685Pin._openState );
+    } else {
+      //_pwm.setPWM(pinNo, 0, pca9685Pin._closeState );
+    }
+  }
+  //displayPinState(pca9685Pin);
+}
 
 void Pca9685::refreshPin(int pinNo, Pca9685Pin pca9685Pin) {
   //todo with adafruit lib implementation
@@ -41,7 +72,7 @@ void Pca9685::refreshPin(int pinNo, Pca9685Pin pca9685Pin) {
   } else {
     //_pwm.setPWM(pinNo, 0, pca9685Pin._closeState );
   }
-  displayPinState(pca9685Pin);
+  //displayPinState(pca9685Pin);
 }
 
 void Pca9685::displayPinState(Pca9685Pin pin) {
@@ -55,16 +86,22 @@ void Pca9685::displayPinState(Pca9685Pin pin) {
   Serial.println();
 }
 
-void Pca9685::displayPinState() {
-  for (int i = 0; i < 16; i++) {
+void Pca9685::displayPca9685PinState() {
+  Serial.print("Board No ");
+  Serial.print(_boardsAddress);
+  Serial.print(" total pins ");
+  Serial.println(this->_totalPins);
+  for (int i = 0; i < this->_totalPins; i++) {
     Pca9685Pin pin = _pca9685PinList[i];
     Serial.println();
+    Serial.print(" Pin ");
+    Serial.print(i);
     Serial.print(" openState ");
-    Serial.println(pin._openState);
+    Serial.print(pin._openState);
     Serial.print(" closeState ");
-    Serial.println(pin._closeState);
+    Serial.print(pin._closeState);
     Serial.print(" isOpen ");
-    Serial.println(pin._isOpen);
+    Serial.print(pin._isOpen);
     Serial.println();
   }
 }
--- CUSTOM-LIBRARY/pac9685-lib/pca9685/Pca9685.h
+++ CUSTOM-LIBRARY/pac9685-lib/pca9685/Pca9685.h
@@ -6,12 +6,13 @@
 
 #ifndef Pca9685_h
 #define Pca9685_h
-#include "Adafruit_PWMServoDriver.h"
+
+#include <Arduino.h>
+//#include "Adafruit_PWMServoDriver.h"
 
 class Pca9685 {
 
   private:
-
     typedef struct {
       int _openState;
       int _closeState;
@@ -19,25 +20,30 @@ class Pca9685 {
     } Pca9685Pin;
 
     int _defaultState;
+    int _totalPins = -1;
     int _boardsAddress;
+
     Pca9685Pin * _pca9685PinList;
     //Adafruit_PWMServoDriver _pwm;
 
-    void initPca9685Board();
+    void initPca9685();
     void refreshPin(int pinNo, Pca9685Pin pca9685Pin);
 
   public:
-    Pca9685(int boardsAddress, int frequency) {
-      _boardsAddress = boardsAddress;
-      _pca9685PinList = new Pca9685Pin[16];
+
+    Pca9685() {
       // _pwm = Adafruit_PWMServoDriver(boardsAddress);
-      // _pwm.setPWMFreq(frequency);
-      initPca9685Board();
+      // _pwm.setPWMFreq(60);
+      initPca9685();
     }
 
-    bool throwSwitch(int pinNo);
-    bool closeSwitch(int pinNo);
-    void displayPinState();
+    void setBoardAddress(int boardsAddress);
+    int getBoardAddress();
+    bool throwSwitchPca9685Pin(int pinNo);
+    bool closeSwitchPca9685Pin(int pinNo);
+    void refreshPca9685Board();
+    void resetPca9685Board();
+    void displayPca9685PinState();
     void displayPinState(Pca9685Pin pin);
 
     ~Pca9685() {
