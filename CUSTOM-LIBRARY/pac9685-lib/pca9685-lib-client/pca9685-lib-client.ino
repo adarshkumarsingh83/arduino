619a9fdadb79d20db1e167df4420a6075247862f
adarshkumarsingh83 <adarshkumarsingh83@gmail.com>
2021-03-06 09:58:33 -0600

    adding pca9685 lib

---
+++ CUSTOM-LIBRARY/pac9685-lib/pca9685-lib-client/pca9685-lib-client.ino
@@ -0,0 +1,29 @@
+
+#include "Pca9685.h"
+#include "Pca9685Board.h"
+
+Pca9685Board *pca9685Board;
+
+#define NO_OF_BOARDS 5
+int MAX_PINS = 0;
+void setup() {
+  Serial.begin(9600);
+  pca9685Board = &Pca9685Board(NO_OF_BOARDS);
+  MAX_PINS = NO_OF_BOARDS * 16;
+}
+
+void loop() {
+  delay(1000);
+  
+  int pinNo = random(0, MAX_PINS);
+  Serial.print("MAX PIN ");
+  Serial.println(MAX_PINS);
+  Serial.print("PIN NO ");
+  Serial.println(pinNo);
+  pca9685Board->throwSwitch(pinNo);
+  pca9685Board->displayPinState();
+  delay(1000);
+  pca9685Board->closeSwitch(pinNo);
+  pca9685Board->displayPinState();
+  delay(1000);
+}
