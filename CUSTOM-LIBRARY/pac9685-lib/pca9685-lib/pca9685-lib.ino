2342f868da11463b220cbb645dcc27bcb2e206c4
adarshkumarsingh83 <adarshkumarsingh83@gmail.com>
2021-03-06 08:52:49 -0600

    adding pac9685-lib

--- CUSTOM-LIBRARY/pac9685-lib/pca9685-lib/pca9685-lib.ino
+++ CUSTOM-LIBRARY/pac9685-lib/pca9685-lib/pca9685-lib.ino
@@ -2,34 +2,28 @@
 #include "Pca9685.h"
 #include "Pca9685Board.h"
 
-int boardAddress[64] = {
-  0x40, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48, 0x49, 0x4A, 0x4B, 0x4C, 0x4D, 0x4E, 0x4F,
-  0x50, 0x51, 0x52, 0x53, 0x54, 0x55, 0x56, 0x57, 0x58, 0x59, 0x5A, 0x5B, 0x5C, 0x5D, 0x5E, 0x5F,
-  0x60, 0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68, 0x69, 0x6A, 0x6B, 0x6C, 0x6D, 0x6E, 0x6F,
-  0x70, 0x71, 0x72, 0x73, 0x74, 0x75, 0x76, 0x77, 0x78, 0x79, 0x7A, 0x7B, 0x7C, 0x7D, 0x7E, 0x7F
-};
-
-//Pca9685 *pca9685;
 Pca9685Board *pca9685Board;
 
+#define NO_OF_BOARDS 5
+int MAX_PINS = 0;
 void setup() {
   Serial.begin(9600);
-  //pca9685 = &Pca9685(0x40);
-  pca9685Board = &Pca9685Board(8);
+  pca9685Board = &Pca9685Board(NO_OF_BOARDS);
+  MAX_PINS = NO_OF_BOARDS * 16;
 }
 
 void loop() {
   delay(1000);
-
-  /*
-    pca9685->displayPca9685PinState();
-    for (int i = 0; i < 16; i++) {
-      pca9685->throwSwitchPca9685Pin(i);
-      delay(1000);
-      pca9685->closeSwitchPca9685Pin(i);
-    }
-  */
-
+  
+  int pinNo = random(0, MAX_PINS);
+  Serial.print("MAX PIN ");
+  Serial.println(MAX_PINS);
+  Serial.print("PIN NO ");
+  Serial.println(pinNo);
+  pca9685Board->throwSwitch(pinNo);
   pca9685Board->displayPinState();
-
+  delay(1000);
+  pca9685Board->closeSwitch(pinNo);
+  pca9685Board->displayPinState();
+  delay(1000);
 }
